71ef57e3b6672a514b224157af6cb4c86add8703
naomi <orest.chupa@gmail.com>
2026-04-03 09:45:09 +0300

    Update hall.py

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -10,7 +10,7 @@ class CinemaHall:
         self,
         movie_name: str,
         customers: list,
-        cleaning_stuff: Cleaner
+        cleaning_staff: Cleaner
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
 
